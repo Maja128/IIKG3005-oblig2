fe900841e4dc6dfa79f6808460794c800a4c16cd
Maja <majame@stud.ntnu.no>
2024-11-08 15:09:21 +0100

    Changed the backend storage account name

--- backend/main.tf
+++ backend/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "maja-rg-backend-tfstate"
-    storage_account_name = "sabetfsen0fmtsoso"
+    storage_account_name = "sabetfs5biilx9phq"
     container_name       = "tfstate"
     key                  = "backend.terraform.tfstate"
   }
