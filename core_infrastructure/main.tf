1fe8542b300daebd4c978e43ebf38c4b284da3ab
Maja <majame@stud.ntnu.no>
2024-11-10 19:35:26 +0100

    Added core infrastructure files for the networking module

---
+++ core_infrastructure/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.7.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "maja-rg-backend-tfstate"
+    storage_account_name = "sabetfs5biilx9phq"
+    container_name       = "tfstate"
+    key                  = "core_infrastructure.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "4832bdd9-c568-497f-af42-3c45d1e16ef6"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_location
+}
+
+module "networking" {
+  source   = "../modules/networking"
+  rg_name   = azurerm_resource_group.rg.name
+  rg_location = azurerm_resource_group.rg.location
+}
---
+++ core_infrastructure/outputs.tf
@@ -0,0 +1,4 @@
+output "subnet_id" {
+  value       = module.networking.subnet_id
+  description = "The ID of the subnet"
+}
