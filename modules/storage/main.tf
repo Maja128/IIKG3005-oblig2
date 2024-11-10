1da9d5d0e16fa522f40f4b4bce8be92170e9112b
Maja <majame@stud.ntnu.no>
2024-11-10 21:47:00 +0100

    Added min_tls_version for the storage account

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -12,6 +12,7 @@ resource "azurerm_storage_account" "sa" {
   location                 = var.rg_location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  min_tls_version          = "TLS1_2"
 }
 
 resource "azurerm_storage_container" "sc" {
